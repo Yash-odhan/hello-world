b84c982db1d06da97966db7330888f33a517ca8d
Yash-odhan <40731645+Yash-odhan@users.noreply.github.com>
2021-10-12 11:19:10 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
     $password = "Yashodhan@123";  
     $db_name = "logins";  
       
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($host, $user, $password);
     if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
