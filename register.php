8d7b58fd41ca7093a83c1317f1283d8304c588c8
Yash-odhan <40731645+Yash-odhan@users.noreply.github.com>
2021-10-12 12:32:22 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php      
+    include('connection.php');  
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];
+
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "insert into login values(username = '$username',password = '$password')";  
+        $result = mysqli_query($con, $sql);  
+if (result === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}          
+
+
+?>  
