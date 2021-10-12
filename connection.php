a68486d95e969b7b1f9cbab21e7880a21e48faa6
Yash-odhan <40731645+Yash-odhan@users.noreply.github.com>
2021-10-12 11:52:39 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,9 +3,15 @@
     $user = "root";  
     $password = '';  
     $db_name = "logins";  
-      
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
+    $mysqli = new mysqli("localhost","root","","logins");
+
+    // Check connection
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    } 
+//     $con = mysqli_connect($host, $user, $password, $db_name);  
+//     if(mysqli_connect_errno()) {  
+//         die("Failed to connect with MySQL: ". mysqli_connect_error());  
+//     }  
 ?>  
